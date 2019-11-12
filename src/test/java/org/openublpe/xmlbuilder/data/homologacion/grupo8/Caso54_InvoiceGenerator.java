ba9b6f27411554259f2d53626a8b804efde23b5e
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-11-12 21:38:52 +0100

    add tests

---
+++ src/test/java/org/openublpe/xmlbuilder/data/homologacion/grupo8/Caso54_InvoiceGenerator.java
@@ -0,0 +1,55 @@
+package org.openublpe.xmlbuilder.data.homologacion.grupo8;
+
+import org.openublpe.xmlbuilder.data.GeneralData;
+import org.openublpe.xmlbuilder.data.InvoiceInputGenerator;
+import org.openublpe.xmlbuilder.data.homologacion.HomologacionUtils;
+import org.openublpe.xmlbuilder.models.input.general.DetalleInputModel;
+import org.openublpe.xmlbuilder.models.input.general.invoice.InvoiceInputModel;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Caso54_InvoiceGenerator implements InvoiceInputGenerator {
+
+    public static InvoiceInputModel INVOICE;
+
+    @Override
+    public InvoiceInputModel getInput() {
+        if (INVOICE == null) {
+            synchronized (this) {
+                if (INVOICE == null) {
+
+                    INVOICE = new InvoiceInputModel();
+                    INVOICE.setSerie("BB11");
+                    INVOICE.setNumero(3);
+
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(2019, Calendar.NOVEMBER, 9, 8, 30, 0);
+                    INVOICE.setFechaEmision(calendar.getTimeInMillis());
+
+                    INVOICE.setFirmante(GeneralData.getFirmante());
+                    INVOICE.setProveedor(GeneralData.getProveedor());
+                    INVOICE.setCliente(GeneralData.getClienteConRUC());
+
+                    INVOICE.setDescuentoGlobal(BigDecimal.ONE);
+
+                    List<DetalleInputModel> detalle = new ArrayList<>();
+                    INVOICE.setDetalle(detalle);
+
+                    for (int i = 0; i < 5; i++) {
+                        DetalleInputModel item = new DetalleInputModel();
+                        detalle.add(item);
+                        item.setDescripcion("Item" + (i + 1));
+                        item.setCantidad(HomologacionUtils.cantidadRandom());
+                        item.setPrecioUnitario(HomologacionUtils.precioUnitarioRandom());
+                    }
+                }
+            }
+        }
+
+        return INVOICE;
+    }
+
+}
---
+++ src/test/java/org/openublpe/xmlbuilder/data/homologacion/grupo8/Caso60_DebitNoteGenerator.java
@@ -0,0 +1,56 @@
+package org.openublpe.xmlbuilder.data.homologacion.grupo8;
+
+import org.openublpe.xmlbuilder.data.DebitNoteInputGenerator;
+import org.openublpe.xmlbuilder.data.homologacion.HomologacionUtils;
+import org.openublpe.xmlbuilder.models.input.general.DetalleInputModel;
+import org.openublpe.xmlbuilder.models.input.general.invoice.InvoiceInputModel;
+import org.openublpe.xmlbuilder.models.input.general.note.debitNote.DebitNoteInputModel;
+import org.openublpe.xmlbuilder.models.ubl.Catalog7;
+
+import java.util.Calendar;
+
+public class Caso60_DebitNoteGenerator implements DebitNoteInputGenerator {
+
+    public static DebitNoteInputModel DEBIT_NOTE;
+
+    @Override
+    public DebitNoteInputModel getInput() {
+        if (DEBIT_NOTE == null) {
+            synchronized (this) {
+                if (DEBIT_NOTE == null) {
+
+                    DEBIT_NOTE = new DebitNoteInputModel();
+                    DEBIT_NOTE.setSerie("FF14");
+                    DEBIT_NOTE.setNumero(1);
+
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(2019, Calendar.NOVEMBER, 9, 8, 30, 0);
+                    DEBIT_NOTE.setFechaEmision(calendar.getTimeInMillis());
+
+                    // get invoice
+                    InvoiceInputModel invoice = Caso53_InvoiceGenerator.INVOICE;
+
+                    // copy
+                    DEBIT_NOTE.setFirmante(invoice.getFirmante());
+                    DEBIT_NOTE.setProveedor(invoice.getProveedor());
+                    DEBIT_NOTE.setCliente(invoice.getCliente());
+                    DEBIT_NOTE.setDetalle(invoice.getDetalle());
+
+                    // No se puede emitir una nota de debito sin tener al menos un detalle GRAVADO
+                    DetalleInputModel item = new DetalleInputModel();
+                    DEBIT_NOTE.getDetalle().add(item);
+                    item.setDescripcion("Item");
+                    item.setCantidad(HomologacionUtils.cantidadRandom());
+                    item.setPrecioUnitario(HomologacionUtils.precioUnitarioRandom());
+                    item.setTipoIGV(Catalog7.GRAVADO_OPERACION_ONEROSA.toString());
+
+                    DEBIT_NOTE.setSerieNumeroInvoiceReference(invoice.getSerie() + "-" + invoice.getNumero());
+                    DEBIT_NOTE.setDescripcionSustentoInvoiceReference("mi descripcion o sustento");
+                }
+            }
+        }
+
+        return DEBIT_NOTE;
+    }
+
+}
