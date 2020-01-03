681b5dc2920c55302313479ea6a9b55c54724165
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-01-03 17:07:50 +0100

    update release script

--- release.sh
+++ release.sh
@@ -11,15 +11,15 @@ mvn versions:set -DnewVersion="$releaseVersion"
 
 git add .
 git commit -m "Release $releaseVersion"
-git push "https://$username:$password@github.com/project-openubl/xml-builder.git"
+git push "https://$username:$password@github.com/project-openubl/xml-builder.git" upstream master
 
 # Create tag and push
 git tag "$releaseVersion"
-git push "https://$username:$password@github.com/project-openubl/xml-builder.git" --tags
+git push "https://$username:$password@github.com/project-openubl/xml-builder.git" --tags upstream
 
 # Create next snapshot
 mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
 git commit -m "Prepare next release $nextVersion-SNAPSHOT"
-git push "https://$username:$password@github.com/project-openubl/xml-builder.git"
+git push "https://$username:$password@github.com/project-openubl/xml-builder.git" upstream master
