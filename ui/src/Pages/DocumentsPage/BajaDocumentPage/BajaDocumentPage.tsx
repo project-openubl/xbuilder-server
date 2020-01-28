ab0c165fdd22b855ffb755350eebee586c9c2b30
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-01-28 14:27:12 +0100

    add Baja

--- ui/src/Pages/DocumentsPage/BajaDocumentPage/BajaDocumentPage.tsx
+++ ui/src/Pages/DocumentsPage/BajaDocumentPage/BajaDocumentPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import DocumentsPageTabs from "../../../PresentationalComponents/DocumentsPageTabs";
 import { XmlBuilderRouterProps } from "../../../models/routerProps";
 import GenericDocument from "../../../SmartComponents/GenericDocument";
+import BajaDocumentForm from "../../../PresentationalComponents/BajaDocumentForm";
+import { BajaDocumentFormData } from "../../../PresentationalComponents/BajaDocumentForm/BajaDocumentForm";
 
 interface StateToProps {}
 
@@ -10,7 +12,7 @@ interface DispatchToProps {}
 interface Props extends StateToProps, DispatchToProps, XmlBuilderRouterProps {}
 
 interface State {
-  form: any;
+  form: BajaDocumentFormData | null;
 }
 
 class BajaDocumentPage extends React.Component<Props, State> {
@@ -21,16 +23,55 @@ class BajaDocumentPage extends React.Component<Props, State> {
     };
   }
 
-  renderForm = () => {
-    return <p>hola</p>;
+  processFormAndGetInputDocument = (form: BajaDocumentFormData) => {
+    const result = {
+      numero: form.numero,
+      fechaEmision: form.fechaEmision ? form.fechaEmision.getTime() : undefined,
+      proveedor: {
+        ruc: form.proveedorRuc,
+        razonSocial: form.proveedorNombreComercial,
+        codigoPostal: form.proveedorCodigoPostal
+      },
+      firmante:
+        form.firmanteRuc && form.firmanteRazonSocial
+          ? {
+              ruc: form.firmanteRuc,
+              razonSocial: form.firmanteRazonSocial
+            }
+          : undefined,
+      tipoDocumentReference: form.tipoDocumentReference,
+      fechaEmisionDocumentReference: form.fechaEmisionDocumentReference
+        ? form.fechaEmisionDocumentReference.getTime()
+        : undefined,
+      serieNumeroDocumentReference: form.serieNumeroDocumentReference,
+      motivoBajaDocumentReference: form.motivoBajaDocumentReference
+    };
+
+    return result;
+  };
+
+  onSubmit = (form: BajaDocumentFormData) => {
+    this.setState({
+      form
+    });
   };
 
   render() {
+    const { form } = this.state;
+
+    let inputDocument = null;
+    if (form) {
+      inputDocument = this.processFormAndGetInputDocument(form);
+    }
+
     return (
       <React.Fragment>
         <DocumentsPageTabs activeKey={1}>
-          <GenericDocument documentType="voided-document" inputDocument={null}>
-            {this.renderForm()}
+          <GenericDocument
+            documentType="voided-document"
+            inputDocument={inputDocument}
+          >
+            <BajaDocumentForm onSubmit={this.onSubmit} />
           </GenericDocument>
         </DocumentsPageTabs>
       </React.Fragment>
