1864aa79801819ca7813296b75c6ee7a97265bac
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-03-29 17:59:37 +0200

    enhance native building

--- extensions/freemarker/deployment/src/main/java/org/openublpe/quarkus/freemarker/deployment/FreemarkerNativeImageProcessor.java
+++ extensions/freemarker/deployment/src/main/java/org/openublpe/quarkus/freemarker/deployment/FreemarkerNativeImageProcessor.java
@@ -18,18 +18,15 @@ package org.openublpe.quarkus.freemarker.deployment;
 
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import org.apache.commons.io.FilenameUtils;
 import org.jboss.logging.Logger;
 import org.openublpe.quarkus.freemarker.FreemarkerBuildConfig;
-import org.openublpe.quarkus.freemarker.QuarkusPathLocationScanner;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -52,65 +49,50 @@ class FreemarkerNativeImageProcessor {
 
     private static final Logger LOGGER = Logger.getLogger(FreemarkerNativeImageProcessor.class);
 
-    @BuildStep
+    @BuildStep(loadsApplicationClasses = true)
     void build(BuildProducer<NativeImageResourceBuildItem> resourceProducer,
-               BuildProducer<GeneratedResourceBuildItem> generatedResourceProducer,
                FreemarkerBuildConfig freemarkerBuildConfig) throws IOException, URISyntaxException {
-        registerNativeImageResources(resourceProducer, generatedResourceProducer, freemarkerBuildConfig);
-    }
-
-    private void registerNativeImageResources(BuildProducer<NativeImageResourceBuildItem> resource,
-                                              BuildProducer<GeneratedResourceBuildItem> generatedResourceProducer,
-                                              FreemarkerBuildConfig freemarkerBuildConfig)
-            throws IOException, URISyntaxException {
-        final List<String> nativeResources = new ArrayList<>();
-        List<String> applicationMigrations = discoverFreemarkerTemplates(freemarkerBuildConfig);
-        nativeResources.addAll(applicationMigrations);
-        // Store freemarker templates in a generated resource that will be accessed later by the Quarkus-Freemarker path scanner
-        String resourcesList = applicationMigrations
-                .stream()
-                .collect(Collectors.joining("\n", "", "\n"));
-        generatedResourceProducer.produce(
-                new GeneratedResourceBuildItem(
-                        QuarkusPathLocationScanner.MIGRATIONS_LIST_FILE,
-                        resourcesList.getBytes(StandardCharsets.UTF_8)));
-        nativeResources.add(QuarkusPathLocationScanner.MIGRATIONS_LIST_FILE);
-        resource.produce(new NativeImageResourceBuildItem(nativeResources.toArray(new String[0])));
+        List<String> templates = discoverFreemarkerTemplates(freemarkerBuildConfig);
+        final List<String> nativeResources = new ArrayList<>(templates);
+        resourceProducer.produce(new NativeImageResourceBuildItem(nativeResources.toArray(new String[0])));
     }
 
     private List<String> discoverFreemarkerTemplates(FreemarkerBuildConfig freemarkerBuildConfig)
             throws IOException, URISyntaxException {
         List<String> resources = new ArrayList<>();
         try {
-            List<String> locations = new ArrayList<>(freemarkerBuildConfig.locations);
-            if (locations.isEmpty()) {
-                locations.add("freemarker/templates");
-            }
+            Set<String> locations = freemarkerBuildConfig.locations.stream()
+                    .map(String::trim)
+                    .filter(element -> !element.isEmpty())
+                    .collect(Collectors.toSet());
             // Locations can be a comma separated list
             for (String location : locations) {
                 // Strip any 'classpath:' protocol prefixes because they are assumed
                 // but not recognized by ClassLoader.getResources()
-                if (location != null && location.startsWith(CLASSPATH_APPLICATION_MIGRATIONS_PROTOCOL + ':')) {
+                if (location.startsWith(CLASSPATH_APPLICATION_MIGRATIONS_PROTOCOL + ':')) {
                     location = location.substring(CLASSPATH_APPLICATION_MIGRATIONS_PROTOCOL.length() + 1);
                 }
+
                 Enumeration<URL> templates = Thread.currentThread().getContextClassLoader().getResources(location);
                 while (templates.hasMoreElements()) {
                     URL path = templates.nextElement();
-                    LOGGER.infov("Adding application freemarker templates in path ''{0}'' using protocol ''{1}''", path.getPath(),
+                    LOGGER.infov("Adding application freemarker templates in path ''{0}'' using protocol ''{1}''",
+                            path.getPath(),
                             path.getProtocol());
                     final Set<String> freemarkerTemplates;
                     if (JAR_APPLICATION_MIGRATIONS_PROTOCOL.equals(path.getProtocol())) {
                         try (final FileSystem fileSystem = initFileSystem(path.toURI())) {
-                            freemarkerTemplates = getFreemarkerTemplatesFromPath(location, path);
+                            freemarkerTemplates = getFreemarkerTemplatesFromPath(freemarkerBuildConfig, location, path);
                         }
                     } else if (FILE_APPLICATION_MIGRATIONS_PROTOCOL.equals(path.getProtocol())) {
-                        freemarkerTemplates = getFreemarkerTemplatesFromPath(location, path);
+                        freemarkerTemplates = getFreemarkerTemplatesFromPath(freemarkerBuildConfig, location, path);
                     } else {
                         LOGGER.warnv(
                                 "Unsupported URL protocol ''{0}'' for path ''{1}''. Freemarker files will not be discovered.",
                                 path.getProtocol(), path.getPath());
                         freemarkerTemplates = null;
                     }
+
                     if (freemarkerTemplates != null) {
                         resources.addAll(freemarkerTemplates);
                     }
@@ -122,15 +104,19 @@ class FreemarkerNativeImageProcessor {
         }
     }
 
-    private Set<String> getFreemarkerTemplatesFromPath(final String location, final URL path)
+    private Set<String> getFreemarkerTemplatesFromPath(FreemarkerBuildConfig freemarkerBuildConfig, final String location, final URL path)
             throws IOException, URISyntaxException {
         try (final Stream<Path> pathStream = Files.walk(Paths.get(path.toURI()))) {
-            return pathStream.filter(Files::isRegularFile)
+            return pathStream
+                    .filter(Files::isRegularFile)
                     .filter(p -> p.getFileName().toString().endsWith(".ftl"))
                     .map(it -> {
                         String file = FilenameUtils.separatorsToUnix(it.toString());
-                        int indexOf = file.lastIndexOf(location);
-                        String substring = file.substring(indexOf);
+                        int indexOf = file.lastIndexOf("/" + location + "/");
+                        if (indexOf == -1) {
+                            indexOf = file.lastIndexOf("/" + location);
+                        }
+                        String substring = file.substring(indexOf + 1);
                         return FilenameUtils.separatorsToUnix(substring);
                     })
                     .peek(it -> LOGGER.debug("Discovered: " + it))
