8d1e95c49e6bb2e0be44ae049acf795de643b15a
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-07-10 07:19:39 +0200

    change docs

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -2,23 +2,5 @@ module.exports = {
   someSidebar: {
     "XBuilder Server": ["introduction", "design_principles", "contributing"],
     "Getting started": ["installation", "example"],
-    Guides: [
-      "concepts",
-      "create_xml",
-      "sign_xml",
-      {
-        Peru: [
-          "peru_supported_documents",
-          "peru_invoice",
-          "peru_creditnote",
-          "peru_debitnote",
-          "peru_voideddocument",
-          "peru_summarydocument",
-          "peru_igv",
-          "peru_icb",
-          "peru_selling_price",
-        ],
-      },
-    ],
   },
 };
